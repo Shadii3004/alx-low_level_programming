b680e43d3c7a8d41bd8c70169f27b86b7f04a949
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-13 14:33:23 -0700

    My sixth task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+print("with proper grammer, but the o outcome is apiece of art,\n");
+
+return (0);
+
+}
