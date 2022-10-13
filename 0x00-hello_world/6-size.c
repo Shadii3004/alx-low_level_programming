911aa2a3a20badec1e2151db059814840b05dbae
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-13 15:12:08 -0700

    My seventh task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-
 char acharacter;
 int ainteger;
 long along;
@@ -21,5 +20,5 @@ printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
-	return (0);
+return (0);
 }  
