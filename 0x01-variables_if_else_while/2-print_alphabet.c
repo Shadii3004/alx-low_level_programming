09c08e90b7f286809b9e9de7bd1bcb3d3ba3f4d0
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-14 13:57:26 -0700

    My third task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,11 @@
 int main(void)
 
 {
-	char c;
+char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-
-		putchar('\n');
-
+for (c = 'a'; c <= 'z'; c++)
+{	putchar(c);
+	putchar('\n');
+}
 	return (0);
 }
