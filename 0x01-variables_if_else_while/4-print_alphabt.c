23592d64ac9f903f7e1a1d3e63aa01e8aca25ef5
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-14 15:44:29 -0700

    My fifth task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,18 +13,18 @@ int main(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-{
+	{
 	if (ch != 'e' && ch != 'q')
-													{
+														{
 															putchar(ch);
-}
+	}
 
 	else
-{
+	{
 
-}
+	}
 
-													}
+														}
 		putchar('\n');
 	return (0);
 }
