7dcf3099296dda4c052c81f14cc90545fbb2d7b1
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-14 21:21:26 -0700

    My tenth task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{ 
-		putchar(i);				
-		if(i != '9')
-		{											putchar(',');
+	putchar(i);				
+	if (i != '9')
+		{														putchar(',');
 			putchar(' ');
 		}
 	}
