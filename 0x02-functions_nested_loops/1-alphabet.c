57663bc7dda8826358947f8b6d86eb0012c68570
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-18 08:26:10 -0700

    My second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - print all alpahbet in lower case followed by a new line
+ */
+
+void print_alphabet(void);
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z' ch++)
+	{
+		_putchar(ch);
+		_putchar('\n')
+	}
+
+	return (0);
+}
