cdc30f447e572985a0a33d822c6bf85b57b8cff2
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-18 12:56:48 -0700

    My seventh task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,12 @@
 /**
  *_abs - computes the absolute value of an integer
  * Return: the absolute value of integer
+ *@n: the integer in question
  */
-
 int _abs(int)
 {
+	int n;
+
 	if (n > 0)
 		return (n);
 	else
