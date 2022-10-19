95dc5ecf9e65b877952bc7edc2f3c4efe8851a3c
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-18 20:14:17 -0700

    My twelfth task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,12 @@ void print_to_98(int n)
 		if (n > 98)
 		{
 			for (count = n; count > 98; --count)
-				printf("%d ", count);
-			_putchar(',');
-			_putchar(' ');
+				printf("%d, ", count);
 		}
 		else
 		{
 			for (count = n; count < 98; ++count)
-				printf("%d ", count);
-		_putchar(',');
-		_putchar(' ');
+				printf("%d, ", count);
 		}
 
 		printf("98\n");
