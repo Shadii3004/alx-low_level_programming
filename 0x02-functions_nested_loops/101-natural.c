b6d519ce2ea7a2d8fea7bbbe4541db2e7eda1c8f
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-19 22:35:04 -0700

    My 101 task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,13 +9,26 @@
 
 int main(void)
 {
-	int sum, num;
+	unsigned long int sum3, sum5, sum;
 
-		for (num = 0; num < 1024; ++num)
+	int = i;
+
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
 		{
-			if ((num % 3 == 0) || (num % 5 == 0))
-				sum += sum;
+			sum3 = sum3 + i;
 		}
-		printf("%d\n", sum);
-		return (0);
+		else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
 }
