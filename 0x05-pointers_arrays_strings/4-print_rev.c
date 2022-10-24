e4a17a0279c06bbec149d50b9054564d2d349030
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-24 09:40:20 -0700

    My fifth task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,7 +15,7 @@ void print_rev(char *s)
 		;
 
 	/*print char from the last index as you decrement*/
-	for (--index; index >= 0; --index)
+	for (index; index >= 0; --index)
 		_putchar(s[index]);
 		_putchar('\n');
 }
