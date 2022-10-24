c8b0b7ee9ea849a1f210f18a6df5b15ac3cfcbc7
Shadii3004 <shadrachmohzo@gmail.com>
2022-10-24 15:29:33 -0700

    My fifth task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,6 +10,10 @@ void print_rev(char *s)
 {
 	int index;
 
+	/*finds the length of string without null character*/
+	for (index = 0; s[index] != '\0'; ++index)
+		;
+
 	/*print char from the last index as you decrement*/
 	for (index = 0; index >= 0; --index)
 		_putchar(s[index]);
