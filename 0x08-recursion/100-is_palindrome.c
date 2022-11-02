78f7fa45ad19bd62a292f0defe3c1750f3cdb6c0
Shadii3004 <shadrachmohzo@gmail.com>
2022-11-02 02:29:16 -0700

    My 100 task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -28,6 +28,7 @@ int comparator(char *s, int n1, int n2)
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
+		else
 			return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
@@ -43,6 +44,6 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	else
+		return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
