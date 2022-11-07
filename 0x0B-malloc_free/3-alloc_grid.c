4ff7a9e2d8633f0b63868165f702cb7096820884
Shadii3004 <shadrachmohzo@gmail.com>
2022-11-06 22:19:42 -0800

    My fourth task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,9 +30,11 @@ int **alloc_grid(int width, int height)
 		if (gridout[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
+			{
 				free(gridout[i]);
 				free(gridout);
 				return (NULL);
+			}
 		}
 	}
 	for (i = 0; i < height; i++)
